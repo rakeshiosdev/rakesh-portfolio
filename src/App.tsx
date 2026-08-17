9a82bd50c29c41d136943506c41eadc15f6a06af
Rakesh Kumar <rakeshiosdev28@gmail.com>
2026-08-17 10:34:37 +0530

    Refactor code and remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import { lazy, Suspense } from "react";
+
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import ScrollProgress from "./components/UI/ScrollProgress";
 import BackToTop from "./components/UI/BackToTop";
-import SectionFallback from "./components/UI/SectionFallback";
-import LazySection from "./components/UI/LazySection";
-
-// const Skills = lazy(() => import("./components/Skills/Skills"));
-// const Projects = lazy(() => import("./components/Projects/Projects"));
-// const Experience = lazy(() => import("./components/Experience/Experience"));
-// const Resume = lazy(() => import("./components/Resume/Resume"));
-// const Footer = lazy(() => import("./components/Footer/Footer"));
 
 import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
@@ -24,34 +16,16 @@ function App() {
       <a href="#main" className="skip-link">
         Skip to main content
       </a>
-
       <ScrollProgress />
       <Navbar />
-
       <main id="main">
         <Hero />
-
-        {/* <LazySection> */}
         <Skills />
-        {/* </LazySection> */}
-
-        {/* <LazySection> */}
         <Projects />
-        {/* </LazySection> */}
-
-        {/* <LazySection> */}
         <Experience />
-        {/* </LazySection> */}
-
-        {/* <LazySection> */}
         <Resume />
-        {/* </LazySection> */}
       </main>
-
-      {/* <LazySection> */}
       <Footer />
-      {/* </LazySection> */}
-
       <BackToTop />
     </>
   );
